373dce81b40c58233fbffc44d7148a89d95ab143
Charles Comstock <dgtized@gmail.com>
2013-04-01 21:35:48 -0500

    install java package

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
                  [com.palletops/pallet "0.8.0-beta.5"]
                  [org.cloudhoist/pallet-vmfest "0.3.0-alpha.2"]
                  [org.clojars.tbatchelli/vboxjws "4.2.6"]
-                 [ch.qos.logback/logback-classic "1.0.11"]]
+                 [ch.qos.logback/logback-classic "1.0.11"]
+
+                 [com.palletops/java-crate "0.8.0-beta.2"]]
   :repositories {"sonatype"
                  "http://oss.sonatype.org/content/repositories/releases"}
   :plugins [[com.palletops/pallet-lein "0.6.0-beta.7"]]
--- src/unit_load/groups/unit_load.clj
+++ src/unit_load/groups/unit_load.clj
@@ -1,7 +1,9 @@
 (ns unit-load.groups.unit-load
   (:require
    [pallet.api :as api]
-   [pallet.crate.automated-admin-user :refer [automated-admin-user]]))
+   [pallet.crate.automated-admin-user :refer [automated-admin-user]]
+   pallet.crate.java
+   pallet.actions))
 
 (def default-node-spec
   (api/node-spec
@@ -11,10 +13,13 @@
 (def base-server
   (api/server-spec
    :phases
-   {:bootstrap (api/plan-fn (automated-admin-user))}))
+   {:bootstrap (api/plan-fn
+                (automated-admin-user)
+                (pallet.actions/package-manager :update))}))
 
 (def unit-load-server
   (api/server-spec
+   :extends [(pallet.crate.java/server-spec {})]
    :phases
    {:configure (api/plan-fn
                 ;; Add your crate class here
