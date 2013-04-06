5350353593627aaecd73b3367270cd3aef75d74e
Charles Comstock <dgtized@gmail.com>
2013-04-06 03:45:13 -0500

    version bump pallet-lein again, and pallet-vmfest

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.palletops/pallet "0.8.0-beta.6"]
-                 [org.cloudhoist/pallet-vmfest "0.3.0-alpha.2"]
+                 [org.cloudhoist/pallet-vmfest "0.3.0-alpha.3"]
                  [org.clojars.tbatchelli/vboxjws "4.2.6"]
                  [ch.qos.logback/logback-classic "1.0.11"]
 
                  [com.palletops/java-crate "0.8.0-beta.2"]]
   :repositories {"sonatype"
                  "http://oss.sonatype.org/content/repositories/releases"}
-  :plugins [[com.palletops/pallet-lein "0.6.0-beta.8"]]
+  :plugins [[com.palletops/pallet-lein "0.6.0-beta.9"]]
   :profiles
   {:pallet
    {:jvm-opts ["-Dvbox.home=/usr/lib/virtualbox"]}})
